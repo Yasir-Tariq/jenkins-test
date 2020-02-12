0771930921b39667863ed841151edccef832ea6c
Yasir-Tariq <ynaseem78@gmail.com>
2020-02-12 14:32:12 -0800

    changed the config file to access root user

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
   */
 
 $host       = "10.0.2.17";
-$username   = "mydbinstance";
+$username   = "roote";
 $password   = "mydbinstance";
 $dbname     = "test"; // will use later
 $dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
